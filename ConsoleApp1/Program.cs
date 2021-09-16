895c77ec9486bef853747475e274aa23ffc2e97b
robertt3kuk <awesome.abaildaev@yandex.kz>
2021-09-16 23:48:00 +0600

    first changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num;
+            int num1;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num + num1);
         }
     }
 }
